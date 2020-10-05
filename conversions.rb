c7a017de8c4daf1716bd405831018c6a62f87207
Cara Zellner <carajeanine@gmail.com>
2020-10-05 22:03:00 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -3,7 +3,7 @@ def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
 
-GRAMS_PER_OUNCE = 28.3495
+GRAMS_PER_OUNCE = 0.0
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
